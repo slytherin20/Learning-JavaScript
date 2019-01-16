8b36c0a8109461bc30abc9d03cdb436cacb6069b
slytherin20 <sonaligupta2904@gmail.com>
2019-01-16 21:17:49 +0530

    Datatype conversion

---
+++ Day-2/Datatype_conversion.js
@@ -0,0 +1,18 @@
+//numeric to string
+
+var str="this number is"+900;
+console.log(str);
+
+var num="360"-60;
+console.log(num);
+
+//String to numeric
+ console.log('1.1'+'1.1');
+ var a=(+'1.1')+(+'1.1');
+ console.log(a);
+ var b=parseInt("10",8); //8 represents radix
+ var c=parseFloat("10.10",10);  //10.10 represents string
+var d=parseInt("12");
+console.log(b);
+console.log(c);
+console.log(d);
