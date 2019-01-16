e930f2e7f69ea4c702896c3281ce27536000f579
slytherin20 <sonaligupta2904@gmail.com>
2019-01-16 20:29:04 +0530

    constants added

---
+++ Day-2/constants.js
@@ -0,0 +1,16 @@
+const PI=3.14;
+console.log(PI);
+PI=43;//Error
+
+
+//Contents of array and object are not protected.
+
+
+const obj={
+  'key':'value'
+};
+obj.key='otherValue';
+console.log(obj.key);
+const arr=['a','b'];
+arr.push('c');
+console.log(arr);
